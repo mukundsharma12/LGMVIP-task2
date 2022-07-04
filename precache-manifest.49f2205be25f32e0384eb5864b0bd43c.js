a61ae5e6594fcb69a525825e94d1a317c436119f
Mukund <mukundrssharma@gmail.com>
2022-07-04 23:51:15 +0530

    Updates

---
+++ precache-manifest.49f2205be25f32e0384eb5864b0bd43c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b7034e23416ecfb3dbe",
+    "url": "/LGMVIP-task2/static/js/main.9b7034e2.chunk.js"
+  },
+  {
+    "revision": "54ffcfc430b1e8f8af61",
+    "url": "/LGMVIP-task2/static/js/1.54ffcfc4.chunk.js"
+  },
+  {
+    "revision": "2522b0bc14539afeb85b",
+    "url": "/LGMVIP-task2/static/js/runtime~main.2522b0bc.js"
+  },
+  {
+    "revision": "9764cddd85b71f5daeeefc989f938953",
+    "url": "/LGMVIP-task2/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/LGMVIP-task2/precache-manifest.49f2205be25f32e0384eb5864b0bd43c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/LGMVIP-task2/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
